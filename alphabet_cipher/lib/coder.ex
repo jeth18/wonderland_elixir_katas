aca090bc6bce86cd52d5a20bbd986ca50e21a96a
Jeth18 <enrique.gomez@busan.io>
2022-03-14 08:23:07 -0600

    ADD - enconde terminado

--- alphabet_cipher/lib/coder.ex
+++ alphabet_cipher/lib/coder.ex
@@ -1,7 +1,18 @@
 defmodule AlphabetCipher.Coder do
 
   def encode(keyword, message) do
-    "encodeme"
+    keyword = lengthKey(keyword, message)
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    cypher = for i <- 0..(String.length(message) - 1), into: [] do
+      {position, _} = :binary.match(alpha, String.at(message, i))
+      {positionKeyword, _} = :binary.match(alpha, String.at(keyword, i))
+      {String.slice(alpha, position..-1) <> String.slice(alpha, 0..(position-1)), positionKeyword}
+    end
+
+    cypher |> Enum.reduce("", fn {str, index}, acc ->
+      acc = acc <> String.at(str, index)
+    end)
   end
 
   def decode(keyword, message) do
@@ -10,5 +21,10 @@ defmodule AlphabetCipher.Coder do
 
   def decipher(cipher, message) do
     "decypherme"
+  end
+
+  def lengthKey(secretKey, message) do
+    secretKey
+      |> String.duplicate(div(String.length(message),String.length(secretKey))+1)
   end
 end
