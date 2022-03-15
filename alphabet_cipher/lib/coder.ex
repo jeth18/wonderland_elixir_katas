86da8a3555a5490927b538b3b2c5c36ed505b29e
jegomez <jegomez@veracruz.gob.mx>
2022-03-14 19:00:20 -0600

    Decode terminado

--- alphabet_cipher/lib/coder.ex
+++ alphabet_cipher/lib/coder.ex
@@ -4,23 +4,32 @@ defmodule AlphabetCipher.Coder do
     keyword = lengthKey(keyword, message)
     alpha = "abcdefghijklmnopqrstuvwxyz"
 
-    cypher = for i <- 0..(String.length(message) - 1), into: [] do
+    for i <- 0..(String.length(message) - 1), into: [] do
       {position, _} = :binary.match(alpha, String.at(message, i))
       {positionKeyword, _} = :binary.match(alpha, String.at(keyword, i))
       {String.slice(alpha, position..-1) <> String.slice(alpha, 0..(position-1)), positionKeyword}
-    end
-
-    cypher |> Enum.reduce("", fn {str, index}, acc ->
-      acc = acc <> String.at(str, index)
+    end |> Enum.reduce("", fn {str, index}, acc ->
+      acc <> String.at(str, index)
     end)
   end
 
   def decode(keyword, message) do
-    "decodeme"
+    keyword = lengthKey(keyword, message)
+    alpha = "abcdefghijklmnopqrstuvwxyz"
+
+    for i <- 0..(String.length(message) - 1), into: [] do
+      {positionKeyword, _} = :binary.match(alpha, String.at(keyword, i))
+      text = String.slice(alpha, positionKeyword..-1) <> String.slice(alpha, 0..(positionKeyword-1))
+      {index, _} = :binary.match(text, String.at(message, i))
+      index
+    end |> Enum.reduce("", fn index, acc ->
+      acc <> String.at(alpha, index)
+    end)
   end
 
+
   def decipher(cipher, message) do
-    "decypherme"
+    "decipher"
   end
 
   def lengthKey(secretKey, message) do
