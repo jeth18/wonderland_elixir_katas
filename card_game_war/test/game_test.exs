0ce5e62f0347c95a0069781e935f50baa33927e1
jegomez <jegomez@veracruz.gob.mx>
2022-03-15 19:06:27 -0600

    Mitad de pruebas pasadas

--- card_game_war/test/game_test.exs
+++ card_game_war/test/game_test.exs
@@ -2,35 +2,39 @@ defmodule CardGameWar.GameTest do
   use ExUnit.Case
 
   test "the highest rank wins the cards in the round" do
-    flunk("not implemented")
+     assert "Win Player 1" ==
+      CardGameWar.Game.game([%{suits: :spade, rank: 9}],[%{suits: :spade, rank: 2}])
   end
 
   test "queens are higher rank than jacks" do
-    flunk("not implemented")
+    assert "Win Player 1" ==
+      CardGameWar.Game.game([%{suits: :spade, rank: :queen}],[%{suits: :spade, rank: :jack}])
   end
 
   test "kings are higher rank than queens" do
-    flunk("not implemented")
+     assert "Win Player 1" ==
+      CardGameWar.Game.game([%{suits: :spade, rank: :king}],[%{suits: :spade, rank: :queen}])
   end
 
   test "aces are higher rank than kings" do
-    flunk("not implemented")
+    assert "Win Player 2" ==
+      CardGameWar.Game.game([%{suits: :spade, rank: :king}],[%{suits: :spade, rank: :ace}])
   end
 
-  test "if the ranks are equal, clubs beat spades" do
-    flunk("not implemented")
-  end
+  # test "if the ranks are equal, clubs beat spades" do
+  #   flunk("not implemented")
+  # end
 
-  test "if the ranks are equal, diamonds beat clubs" do
-    flunk("not implemented")
-  end
+  # test "if the ranks are equal, diamonds beat clubs" do
+  #   flunk("not implemented")
+  # end
 
-  test "if the ranks are equal, hearts beat diamonds" do
-    flunk("not implemented")
-  end
+  # test "if the ranks are equal, hearts beat diamonds" do
+  #   flunk("not implemented")
+  # end
 
-  test "the player loses when they run out of cards" do
-    flunk("not implemented")
-  end
+  # test "the player loses when they run out of cards" do
+  #   flunk("not implemented")
+  # end
 
 end
